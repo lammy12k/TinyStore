2685a7c2a3b315b31b5282b08193c0bde76919be
Kyle <kylef@myyahoo.com>
2026-09-10 12:38:56 -0400

    Updated RecordEnvelope (again for fixing JavaDocs) and fixed publishing to use the lowercase case-sensitive name.

--- src/main/java/com/kyfstore/tinystore/RecordEnvelope.java
+++ src/main/java/com/kyfstore/tinystore/RecordEnvelope.java
@@ -13,6 +13,9 @@ public class RecordEnvelope implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Raw serialized data payload.
+     */
     private final byte[] payload;
 
     /**
